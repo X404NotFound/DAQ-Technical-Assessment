65927eb4118fa5990c91a3a50e6b50dcaa1cb705
Yiwen Liao <z5342335@ad.unsw.edu.au>
2023-08-30 14:47:01 +1000

    lint and test fixed, hopefully

--- battery-ui/tests/ui.test.ts
+++ battery-ui/tests/ui.test.ts
@@ -1,5 +1,5 @@
-describe("dummy test for ui", () => {
-  test("dummy test", () => {
+describe('dummy test for ui', () => {
+    test('dummy test', () => {
         expect(true).toBe(true);
-  });
+    });
 });
